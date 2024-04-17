13147d1e256da3de44ff2e99a09a0d85d285ee99
Andrew <andrew.colinet@alum.utoronto.ca>
2024-04-17 19:36:35 -0400

    Adding more to Chapter 2 workbook.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Overview
 
-Included in this repository is a project to learn about the natural language processing library spaCy. We proceed using an [online introduction](https://course.spacy.io/en/)
+Included in this repository is a project to learn about the natural language processing library spaCy. We proceed using an [online introduction](https://course.spacy.io/en/). I will complete all provided exercises in order to reinforce understanding.
--- chapter_2/chapter_2_workbook.py
+++ chapter_2/chapter_2_workbook.py
@@ -201,10 +201,107 @@ print("\n")
 
 
 
+# overwrite nlp object with medium sized pipeline
+nlp = spacy.load("en_core_web_md")  # need medium or large sized pipeline for similarity scoring
+
+# compare two documents
+doc1 = nlp("I like fast food")
+doc2 = nlp("I like pizza")
+print(f"Similarity score between documents doc1 and doc2: {doc1.similarity(doc2)}")
+
+
+# compare two tokens
+doc = nlp("I like pizza and pasta")
+token1 = doc[2]
+token2 = doc[4]
+print(f"Similarity score between tokens token1 and token2: {token1.similarity(token2)}")
+
+
+
+# compare a document with a token
+doc = nlp("I like pizza")
+token = nlp("soap")[0]
+
+# print similarity of token to doc
+print(f"Similarity of doc and token: {doc.similarity(token)}")
+
+
+
+# compare a span with a document
+span = nlp("I like pizza and pasta")[2:5]
+doc = nlp("Wendy's sells burgers")
+
+# print similarity of span and doc
+print(f"Similarity of span and doc: {span.similarity(doc)}")
+
+
+
+# overwrite doc
+doc = nlp("I have a banana")
+
+# access the vector via the token.vector attribute
+print(doc[3].vector)
 
 
 
 
+# print new line to separate
+print("\n")
+
+
+
+
+# overwrite nlp object with medium sized pipeline
+nlp = spacy.load("en_core_web_md")
+
+# overwrite doc
+doc = nlp("Two bananas in pyjamas")
+
+# get the vector for the token 'bananas'
+bananas_vector = doc[1].vector
+print(bananas_vector)
+
+
+
+
+# print new line to separate
+print("\n")
+
+
+
+
+# overwrite nlp object with medium sized pipeline
+nlp = spacy.load("en_core_web_md")
+
+# get docs
+doc1 = nlp("It's a warm summer day")
+doc2 = nlp("It's sunny outside")
+
+# get the similarity of doc1 and doc2
+similarity = doc1.similarity(doc2)
+print(f"Similarity of the documents doc1 and doc2: {similarity}")
+
+
+# get tokens from doc
+doc = nlp("TV and books")
+token1, token2 = doc[0], doc[2]  # get 'TV' and 'books'
+
+# get the similarity of the tokens 'TV' and 'books'
+similarity = token1.similarity(token2)
+print(f"Similarity of the tokens token1 and token2: {similarity}")
+
+
+
+# create new document
+doc = nlp("This was a great restaurant. Afterwards, we went to a really nice bar.")
+
+# create span for 'great restaurant' and 'really nice bar'
+span1 = Span(doc, 3, 5)
+span2 = Span(doc, 12, 15)
+
+# get the similarity of the spans
+similarity = span1.similarity(span2)
+print(f"Similarity of the spans span1 and span2: {similarity}")
 
 
 
